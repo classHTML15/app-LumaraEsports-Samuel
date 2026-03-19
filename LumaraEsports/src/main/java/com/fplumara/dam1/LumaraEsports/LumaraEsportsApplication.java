7d1ec23a24de8b5cc345d3b63a42bfb09cab3f6e
Samu7526 <samueljosue312005@gmail.com>
2026-03-19 12:43:33 +0100

    First commit

---
+++ LumaraEsports/src/main/java/com/fplumara/dam1/LumaraEsports/LumaraEsportsApplication.java
@@ -0,0 +1,13 @@
+package com.fplumara.dam1.LumaraEsports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumaraEsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumaraEsportsApplication.class, args);
+	}
+
+}
---
+++ LumaraEsports/src/test/java/com/fplumara/dam1/LumaraEsports/LumaraEsportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fplumara.dam1.LumaraEsports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumaraEsportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
