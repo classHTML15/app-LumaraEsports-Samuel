2c8ad0ad58233e6e1c90e5666d1a61641b07fe93
classHTML15 <samueljosue312005@gmail.com>
2026-03-22 00:37:44 +0100

    Creacion-model-y-exception

---
+++ src/main/java/com/fplumara/dam1/LumaraEsports/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package com.fplumara.dam1.LumaraEsports.exceptions;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/fplumara/dam1/LumaraEsports/model/TorneoConEquipo.java
@@ -0,0 +1,19 @@
+package com.fplumara.dam1.LumaraEsports.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TorneoConEquipo {
+
+    private Long id;
+    private String nombreTorneo;
+    private String ciudad;
+    private BigDecimal premio;
+    private String nombreEquipo;
+}
